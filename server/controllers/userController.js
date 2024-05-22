ec0865ab36938b4eed7942dbb6eb67c048144f03
Aaqil yousuf <aaqilyousuf@gmail.com>
2024-05-22 19:39:46 -0400

    jwt implementation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import express from "express";
 import User from "../models/User.js";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const Register = async (req, res) => {
   const { userName, email, password } = req.body;
@@ -26,22 +27,38 @@ export const Register = async (req, res) => {
 };
 
 export const Login = async (req, res) => {
-  const { userName, email, password } = req.body;
+  const { email, password } = req.body;
   try {
-    const existingUser = await User.findOne({ email });
-    if (!existingUser) {
+    const loginUser = await User.findOne({ email });
+    if (!loginUser) {
       return res.status(404).json({ msg: "User Not Found!!!" });
     }
-    console.log(existingUser);
-    const isPassword = await bcrypt.compare(password, existingUser.password);
+    console.log(loginUser);
+    const isPassword = await bcrypt.compare(password, loginUser.password);
     if (isPassword) {
+      const token = jwt.sign(
+        {
+          userName: loginUser.userName,
+          email: loginUser.email,
+          createdAt: Date.now(),
+        },
+        process.env.JWT_KEY
+      );
+      console.log(token);
       return res
         .status(200)
-        .json({ msg: "Login into your account successfully!!!" });
+        .json({ msg: "Login into your account successfully!!!", token: token });
     }
   } catch (err) {
     return res.status(401).json({ msg: "Email or password wrong" });
   }
 };
 
-export const Logout = async (req, res) => {};
+export const Logout = async (req, res) => {
+  res.status(200).json({ msg: "response successfull" });
+};
+
+export const getScholarship = async (req, res) => {
+  try {
+  } catch (err) {}
+};
